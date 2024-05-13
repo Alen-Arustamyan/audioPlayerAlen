214979a5307cdf23a8013759c6df244814043de0
Alen-Arustamyan <alen.arustamyan@tumo.org>
2024-05-13 17:18:10 +0400

    final

--- Folder 2/script.js
+++ Folder 2/script.js
@@ -17,25 +17,82 @@ let song = new Audio()
 window.onload = function () {
     playSong()
 }
-let currentSong =0
+let currentSong = 0
 function playSong() {
     song.src = data.song[currentSong]
     let songTitle = document.getElementById("songTitle")
     songTitle.textContent = data.title[currentSong]
     let img = document.getElementsByClassName("row1")
-    img[0].style.backgroundImage = "url("+ data.poster[currentSong] +")"
+    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")"
     let main = document.getElementsByClassName("main")
-    main[0].style.backgroundImage = "url("+ data.poster[currentSong] +")"
-    
+    main[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")"
+
 }
-function playOrPause(){
+function playOrPause() {
     let play = document.getElementById("play")
 
-    if(song.paused){
+    if (song.paused) {
         song.play()
-        play.src= "images/pause.png"
-    }else{
+        play.src = "images/pause.png"
+    } else {
         song.pause()
         play.src = "images/play-button-arrowhead.png"
     }
+}
+
+song.addEventListener("timeupdate",function(){
+    // console.log(song.currentTime);
+    // console.log(song.duration);
+    let fill = document.getElementsByClassName("fill")
+    let position = song.currentTime / song.duration
+
+    fill[0].style.marginLeft = position * 100 + "%"
+
+    convertTime(song.currentTime)
+
+    if(song.ended){
+        next()
+        
+    }
+})
+
+function convertTime(seconds){
+     currentTime = document.getElementsByClassName("currentTime")
+
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    currentTime[0].textContent = min + ":" + sec
+
+ totalTime(song.duration)   
+}
+function totalTime(seconds){
+    let min = Math.floor(seconds / 60)
+    let sec = Math.floor(seconds % 60)
+
+    min = (min < 10) ? "0" + min : min;
+    sec = (sec < 10) ? "0" + sec : sec;
+
+    currentTime[0].textContent += " / " + min + ":" + sec
+}
+
+function next(){
+    currentSong++
+    if(currentSong>= data.song.length){
+        currentSong=0
+    }
+    playSong()
+    play.src = "images/pause.png"
+}
+
+function prev(){
+    currentSong--
+    if(currentSong< 0){
+        currentSong = data.song.length - 1
+    }
+    playSong()
+    play.src = "images/pause.png"
 }

18959f418aabd8bff51bc6afd547de26b782a6fc
Alen-Arustamyan <alen.arustamyan@tumo.org>
2024-05-18 10:47:12 +0400

    final project

--- Folder 2/script.js
+++ Folder 2/script.js
@@ -1,14 +1,15 @@
 let data = {
     title: ["Luyser",
-        "Там ревели горы",
+        "Спой",
         "When I Win"],
 
-    song: ["music/Karen Gevorgyan - Luyser, Luyser.mp3",
-        "MiyaGi & Andy Panda .mp3",
-        "MiyaGi & Endspiel - When I Win.mp3"],
+    song: [
+        "music/Karen Gevorgyan - Luyser, Luyser.mp3",
+        "music/A.V.G feat. MACAN - spoy.mp3",
+        "music/MiyaGi & Endspiel - When I Win.mp3"],
 
     poster: ["https://source.boomplaymusic.com/group1/M1D/82/1E/rBEeMV7c6IeAKCFgAADA9DG6ay0044.jpg",
-        "https://i.ytimg.com/vi/S0WLFGYSGdg/sddefault.jpg",
+        "https://cdn.promodj.com/afs/19b6cf3bd4a7bf9471f98ce268fb370912%3Aresize%3A640x480%3Afill%3Affffff%3A5cab1c",
         "https://lastfm.freetls.fastly.net/i/u/500x500/23bf00d9af5797c22d9975d1506008d6.jpg"]
 }
 
@@ -85,6 +86,7 @@ function next(){
         currentSong=0
     }
     playSong()
+    song.play()
     play.src = "images/pause.png"
 }
 
@@ -94,7 +96,24 @@ function prev(){
         currentSong = data.song.length - 1
     }
     playSong()
+    song.play()
     play.src = "images/pause.png"
 }
 
-///
+console.log(song)
+function mute(){
+    let mute = document.getElementById("mute")
+    if(song.muted){
+        mute.src = "images/volume.png"
+        song.muted = false
+    }else{
+        mute.src = "images/volume-mute.png"
+        song.muted = true
+    }
+}
+function decrease(){
+    song.volume-=0.2
+}
+function increase(){
+    song.volume+=0.2
+}
